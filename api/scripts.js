7cad9152f5010d631e05c90aa07e8c973c967835
namkang321 <namkang_chutisilp@hotmail.com>
2020-01-09 19:38:01 +0700

    test script src ref

--- api/scripts.js
+++ api/scripts.js
@@ -1,11 +1,12 @@
-$(document).ready(function() {
-    var userapi = 'https://login-register-update-app.herokuapp.com/user'
-    $.getJSON(userapi, function(data) {
-        console.log(data)
-        if (data) {
-            $('#input-firstname').val(data.firstname)
-            $('#input-lastname').val(data.lastname)
-        } else {
-        }
-    })
-})
+// $(document).ready(function() {
+//     var userapi = 'https://login-register-update-app.herokuapp.com/user'
+//     $.getJSON(userapi, function(data) {
+//         console.log(data)
+//         if (data) {
+//             $('#input-firstname').val(data.firstname)
+//             $('#input-lastname').val(data.lastname)
+//         } else {
+//         }
+//     })
+// })
+console.log('Hello')
